a39d7dc2515ac47daa3e6f0acd9faba9b07e6e64
Zach Swanson <zach.swanson@doane.edu>
2024-09-26 02:51:57 -0500

    Finished part 1 of the assignment.

--- homework1.m
+++ homework1.m
@@ -22,47 +22,69 @@ fprintf('Training data size: %d\n', length(train_x));
 fprintf('Test data size: %d\n', length(test_x));
 
 n_input = 1;
-n_hidden = 5;
+% n_hidden = 5;
 n_output = 1;
-learning_gain = 0.01; % [0.01, 1]
-momentum_gain = 0.001;  % [0, 1]
+% learning_gain = 0.01; % [0.01, 1]
+% momentum_gain = 0.001;  % [0, 1]
 
-lg = [0.01, 0.1, 0.5, 0.7, 0.9,  1];
-mg = [0.0001, 0.001, 0.01, 0.1, 0.5, 0.7, 0.9, 1];
+% learning_gain = [0.01, 0.1, 0.5, 0.7, 0.9,  1];
+% momentum_gain = [0, 0.0001, 0.001, 0.01, 0.1, 0.5, 0.7, 0.9, 1];
 
-mlp = multilayer_perceptron(n_input, n_hidden, n_output, learning_gain, momentum_gain);
+learning_gain = [0.01];
+momentum_gain = [0.1];
 
-epochs = [1:10000];
-mse = zeros(1, length(epochs));
+for n_hidden = [3, 4, 6, 7, 8, 9, 10]
+    for i = learning_gain
+        for j = momentum_gain
+            fprintf('--------------------------------------------------------------------------------\n');
+            fprintf('Training model with n_hidden = %d, learning_gain = %.3f, momentum_gain = %.4f\n', n_hidden, i, j);
+            mlp = multilayer_perceptron(n_input, n_hidden, n_output, i, j);
 
-for i = epochs
-    mse(i) = mlp.train(train_x, train_y);
+            epochs = [1:10000];
+            mse = zeros(1, length(epochs));
 
-    fprintf('Epoch %d: MSE = %f\n', i, mse(i));
+            for k = epochs
+                mse(k) = mlp.train(train_x, train_y);
+                if mod(k, 1000) == 0
+                    fprintf('Epoch %d: MSE = %f\n', k, mse(k));
+                end
+            end
+
+            min_mse = min(mse);
+            avg_mse = mean(mse);
+            test_mse = mlp.evaluate(test_x, test_y);
+
+            % plot the MSE over the epochs on a log scale... write plot to file
+            figure("visible", "off");
+            semilogy(epochs, mse);
+            xlabel('Epochs');
+            ylabel('MSE');
+            new_title = sprintf('MSE over epochs\nn = %d, learning gain = %.3f, momentum gain = %.4f', n_hidden, i, j);
+            title(new_title);
+            filename = sprintf('figures/mse_%d_%.3f_%.4f.png', n_hidden, i, j);
+            saveas(gcf, filename);
+
+
+            % plot the full dataset against the predictions
+            y_hat = mlp.forward(test_x);
+            figure("visible", "off");
+            plot(test_x, test_y, 'r', test_x, y_hat, 'b');
+            xlabel('x');
+            ylabel('y');
+            new_title = sprintf('Test-data vs Predictions\nn = %d, learning gain = %.3f, momentum gain = %.4f', n_hidden, i, j);
+            title(new_title);
+            filename = sprintf('figures/function_fitting_%d_%.3f_%.4f.png', n_hidden, i, j);
+            saveas(gcf, filename);
+
+            % append the min and avg MSE to the file
+            filename = 'mse_results.csv';
+            fileID = fopen(filename, 'a');
+            fprintf(fileID, '%d,%.3f,%.4f,%.8f,%.8f,%.8f\n', n_hidden, i, j, min_mse, avg_mse, test_mse);
+            fclose(fileID);
+
+            % save hidden and output layer weights
+            filename = sprintf('models/model_%d_%.3f_%.4f.mat', n_hidden, i, j);
+            save(filename, 'mlp');
+        end
+    end
 end
-
-min_mse = min(mse);
-avg_mse = mean(mse);
-
-% plot the MSE over the epochs on a log scale... write plot to file
-figure("visible", "off");
-semilogy(epochs, mse);
-xlabel('Epochs');
-ylabel('MSE');
-new_title = sprintf('MSE over epochs\nn = %d, learning gain = %.3f, momentum gain = %.3f', n_hidden, learning_gain, momentum_gain);
-title(new_title);
-filename = sprintf('figures/mse_%d_%.3f_%.3f.png', n_hidden, learning_gain, momentum_gain);
-saveas(gcf, filename);
-
-
-% plot the full dataset against the predictions
-y_hat = mlp.forward(test_x);
-figure("visible", "off");
-plot(test_x, test_y, 'r', test_x, y_hat, 'b');
-xlabel('x');
-ylabel('y');
-new_title = sprintf('Test-data vs Predictions\nn = %d, learning gain = %.3f, momentum gain = %.3f', n_hidden, learning_gain, momentum_gain);
-title(new_title);
-filename = sprintf('figures/function_fitting_%d_%f_%f.png', n_hidden, learning_gain, momentum_gain);
-saveas(gcf, filename);
-
